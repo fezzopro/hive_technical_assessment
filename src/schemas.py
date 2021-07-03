5e4da200604353153586920dc8b4f576e58e34b8
Fezzopro <fezzopro@gmail.com>
2021-07-03 12:37:29 +0200

    Changing the Database Configurations, And Creating the schemas automatically. Even though it not a good idea ☺☻

--- src/schemas.py
+++ src/schemas.py
@@ -1,14 +1,12 @@
 from uuid import UUID
 from datetime import date
-import datetime
-from sqlalchemy.sql.sqltypes import Integer, String
 from pydantic import BaseModel
 
 class Customer(BaseModel):
     id: UUID
     firstName: str
     lastName: str
-    dateOfBirth: datetime.date
+    dateOfBirth: date
 class Account(BaseModel):
     id: UUID
     accountNumber: str
